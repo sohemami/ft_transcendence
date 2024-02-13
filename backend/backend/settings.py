7abda67d1d7808dc1a3b8c99060f20bd24620da7
asanson <asanson@student.42.fr>
2024-02-13 15:03:52 +0100

    data test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,12 +84,12 @@ import os
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trans',
+        'USER': 'Transcendence',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
